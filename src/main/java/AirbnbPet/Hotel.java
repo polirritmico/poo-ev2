58d764950af77b31809d42e6cbbe4cf4ec540429
polirritmico <ebray187@gmail.com>
2025-10-31 15:36:47 -0300

    feat(hotel): base approach to implement registerPet flow

--- src/main/java/AirbnbPet/Hotel.java
+++ src/main/java/AirbnbPet/Hotel.java
@@ -2,9 +2,36 @@ package AirbnbPet;
 
 import AirbnbPet.Pets.Pet;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Hotel {
     private Map<String, Pet> guests = new HashMap<>();
+
+    private Boolean isPetCodeAlreadyRegister(String checkCode) {
+        return false;
+    }
+
+    private List<String> validate(Pet visitor) {
+        List<String> output = new ArrayList<String>();
+        return output;
+    }
+
+    private void showErrorMessages(List<String> messages) {
+    }
+
+    private void checkInGuest(Pet guest) {
+    }
+
+    public boolean registerPet(Pet incomingGuest) {
+        List<String> validationResults = validate(incomingGuest);
+        if (!validationResults.isEmpty()) {
+            showErrorMessages(validationResults);
+        }
+
+        checkInGuest(incomingGuest);
+        return true;
+    }
 }
