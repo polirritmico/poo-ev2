04585467de9c82a50bda84fdfa5ac2739ed53187
polirritmico <ebray187@gmail.com>
2025-10-31 17:27:16 -0300

    tests(dog): add exercise registry tests

--- src/test/java/AirbnbPet/DogTest.java
+++ src/test/java/AirbnbPet/DogTest.java
@@ -4,18 +4,65 @@ import AirbnbPet.Pets.Dog;
 import org.junit.jupiter.api.Test;
 
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DogTest {
+    private Dog newMockDog() {
+        return new Dog(
+            "mockId-123",
+            true,
+            "Mock Dog",
+            6,
+            5,
+            5,
+            0
+        );
+    }
+
     @Test
     void shouldSetAndGetPassedDogId() {
         String caseValue = "asdf1234";
         String expected = "asdf1234";
 
-        Dog dog = new Dog();
+        Dog dog = newMockDog();
         dog.setId(caseValue);
         String output = dog.getId();
 
         assertEquals(expected, output);
+    }
+
+    @Test
+    void shouldRegisterExerciseSessions() {
+        Dog dog = newMockDog();
+        int expected = 2;
+        LocalDate today = LocalDate.now();
+        assertEquals(0, dog.getDailyExerciseSessions());
+
+        dog.registerNewExerciseSession(today);
+        int output = dog.registerNewExerciseSession(today);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    void shouldResetPreviousDayRegisterExerciseSessions() {
+        int expectedStartingSessions = 0;
+        int expectedYesterdaySessions = 2;
+        int expectedTodaySessions = 1;
+
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+
+        Dog dog = newMockDog();
+        int outputStartingSessions = dog.getDailyExerciseSessions();
+        dog.registerNewExerciseSession(yesterday);
+        int outputYesterdaySessions = dog.registerNewExerciseSession(yesterday);
+        int outputTodaySessions = dog.registerNewExerciseSession(today);
+
+        assertEquals(expectedStartingSessions, outputStartingSessions);
+        assertEquals(expectedYesterdaySessions, outputYesterdaySessions);
+        assertEquals(expectedTodaySessions, outputTodaySessions);
     }
 }
