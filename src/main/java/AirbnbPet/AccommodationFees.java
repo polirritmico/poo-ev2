ac6cb6aeadfafb7df9760cc643e9f7ec15ae2f39
polirritmico <ebray187@gmail.com>
2025-10-31 13:30:22 -0300

    feat(AccommodationFees): add base interface according to intructions

--- src/main/java/AirbnbPet/AccommodationFees.java
+++ src/main/java/AirbnbPet/AccommodationFees.java
@@ -2,14 +2,18 @@ package AirbnbPet;
 
 import java.util.Map;
 
+// NOTE: Solo sigo las instrucciones al pie de la letra, pero encuentro que este
+// no es el patrón correcto para utilizar interfaces. Que puedan almacenar datos
+// no implica que se deba hacer. La idea es definir contratos de comportamiento,
+// no almacenar valores y mezclar responsabilidades.
 public interface AccommodationFees {
-    static final int VALOR_DIA_ALOJAMIENTO = 25_000;
+    int VALOR_DIA_ALOJAMIENTO = 25_000;
 
-    static final Map<String, Integer> dailyPercentageFeesByPetType = Map.of(
-            "Dog", 7,
-            "Cat", 5,
-            "Rabbit", -7
+    Map<String, Integer> percentageIncreaseFeesByPetType = Map.of(
+        "Dog", 7,
+        "Cat", 5,
+        "Rabbit", -7
     );
 
-    int calculateValue(int days);
+    int calculateValue(String petType, int days);
 }
