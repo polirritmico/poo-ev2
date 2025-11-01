5a0e1c12c6c1d2fc00a9544d9a4fc09e17967ab4
polirritmico <ebray187@gmail.com>
2025-10-31 22:33:58 -0300

    feat(Rabbit): update constructor and add hasNeighbors parameter

--- src/main/java/AirbnbPet/Pets/Rabbit.java
+++ src/main/java/AirbnbPet/Pets/Rabbit.java
@@ -6,17 +6,23 @@ import java.util.List;
 public class Rabbit extends Pet {
     // identify by eaten food
     private String foodType;
+    private boolean hasNeighbors;
 
     public Rabbit() {}
 
-    public Rabbit(String id, String name, double weightKg, int ageYears, int ageMonths, boolean requiresNightSupervision, String foodType) {
+    public Rabbit(String id, String name, double weightKg, int ageYears, int ageMonths, boolean requiresNightSupervision, String foodType, boolean hasNeighbors) {
         super(id, name, weightKg, ageYears, ageMonths, requiresNightSupervision);
         this.foodType = foodType;
+        this.hasNeighbors = hasNeighbors;
     }
 
     public String getFoodType() {return foodType;}
 
     public void setFoodType(String foodType) {this.foodType = foodType;}
+
+    public boolean isHasNeighbors() {return hasNeighbors;}
+
+    public void setHasNeighbors(boolean hasNeighbors) {this.hasNeighbors = hasNeighbors;}
 
     @Override
     public List<String> getData() {
