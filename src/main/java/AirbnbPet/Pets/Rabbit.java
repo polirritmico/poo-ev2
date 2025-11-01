c92f11ac2682cdad87df2de64947f4690919bc7c
polirritmico <ebray187@gmail.com>
2025-10-31 22:27:37 -0300

    feat(Rabbit): implement getData function

--- src/main/java/AirbnbPet/Pets/Rabbit.java
+++ src/main/java/AirbnbPet/Pets/Rabbit.java
@@ -21,6 +21,13 @@ public class Rabbit extends Pet {
     @Override
     public List<String> getData() {
         List<String> res = new ArrayList<>();
+        res.add("[Rabbit Data]");
+        res.add("ID:\t" + getId());
+        res.add("Name:\t" + getName());
+        res.add("Weight:\t" + getWeightKg() + " kg");
+        res.add("Age:\t" + getAgeYears() + " years, " + getAgeMonths() + " months");
+        res.add("Food type:\t" + getFoodType());
+        res.add("Requires night supervision:\t" + requiresNightSupervision());
         return res;
     }
 
