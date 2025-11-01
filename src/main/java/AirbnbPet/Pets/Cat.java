3457a1ea18cb2d499aea9588e62c7bd2beb4dd86
polirritmico <ebray187@gmail.com>
2025-10-31 22:26:35 -0300

    feat(Cat): implement getData & pedigree setter/getter

--- src/main/java/AirbnbPet/Pets/Cat.java
+++ src/main/java/AirbnbPet/Pets/Cat.java
@@ -14,9 +14,24 @@ public class Cat extends Pet {
         this.pedigree = pedigree;
     }
 
+    public String getPedigree() {
+        return pedigree;
+    }
+
+    public void setPedigree(String pedigree) {
+        this.pedigree = pedigree;
+    }
+
     @Override
     public List<String> getData() {
         List<String> res = new ArrayList<>();
+        res.add("[Cat Data]");
+        res.add("ID:\t" + getId());
+        res.add("Name:\t" + getName());
+        res.add("Weight:\t" + getWeightKg() + " kg");
+        res.add("Age:\t" + getAgeYears() + " years, " + getAgeMonths() + " months");
+        res.add("Pedigree:\t" + getPedigree());
+        res.add("Requires night supervision:\t" + requiresNightSupervision());
         return res;
     }
 
